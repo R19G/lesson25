f908587bb2d0fbe866f038a277f9a4fcc27b1535
R19G <guseynov_rustam2017@mail.ru>
2024-08-31 22:12:20 +0300

    Update module7_2.py

--- module7_2.py
+++ module7_2.py
@@ -5,9 +5,9 @@ from pprint import pprint
 def custom_write(file_name, strings):
     strings_positions = {}
     with open(file_name, 'w', encoding='utf-8') as f:
-        for i, string in enumerate(strings):
-            f.write(string + '\n')
+        for i, string in enumerate(strings, 1):
             strings_positions[i, f.tell()] = string
+            f.write(string + '\n')
     f.close()
     return strings_positions
 
